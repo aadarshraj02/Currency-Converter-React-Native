3b28c49b0df7456ac6f8398fb028b0c648ba997c
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-11 09:43:01 +0530

    added a function buttonPressed for conversion

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,29 @@ const App = () => {
   const [resultValue, setResultValue] = useState('');
   const [targetCurrency, setTargetCurrency] = useState('');
 
+  const buttonPressed = (targetValue: Currency) => {
+    if (!inputValue) {
+      return Snackbar.show({
+        text: 'Enter a value to convert',
+        backgroundColor: '#ea7773',
+        textColor: '#000',
+      });
+    }
+    const inputAmount = parseFloat(inputValue);
+    if (!isNaN(inputAmount)) {
+      const convertedValue = inputAmount * targetValue.value;
+      const result = `${targetValue.symbol}${convertedValue.toFixed(2)}`;
+      setResultValue(result);
+      setTargetCurrency(targetValue.name);
+    } else {
+      return Snackbar.show({
+        text: 'Not a valid Number',
+        backgroundColor: '#ea7773',
+        textColor: '#000',
+      });
+    }
+  };
+
   return (
     <SafeAreaView>
       <StatusBar />
